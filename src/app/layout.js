b027e77c2c3c8f18a3519cf9b953382fbc11a5b1
rabbounibs002 <rabbounibusinesssarl002@gmail.com>
2026-09-03 12:17:16 +0200

    Add root layout

---
+++ src/app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Rabbouni Business SARL',
+  description: 'Site officiel de Rabbouni Business SARL',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
